d1f82b7f5184a2afacaf95ec2f33afe648fc1690
Yildiz-BO <amamou969@gmail.com>
2020-07-25 22:48:50 +0100

    Reactive Forms Part 2

--- angular/conFusion/src/app/contact/contact.component.ts
+++ angular/conFusion/src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
 import { from } from 'rxjs';
@@ -11,25 +11,37 @@ export class ContactComponent implements OnInit {
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
+  @ViewChild('fform') feedbackFormDirective;
   constructor(private fb: FormBuilder) { 
     this.createForm()
   }
-  createForm(){
-    this.feedbackForm = this.fb.group( {
+  createForm() {
+    this.feedbackForm = this.fb.group({
+      firstname: ['', Validators.required ],
+      lastname: ['', Validators.required ],
+      telnum: ['', Validators.required ],
+      email: ['', Validators.required ],
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+  }
+
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset({
       firstname: '',
       lastname: '',
-      telnum: 0,
+      telnum: '',
       email: '',
       agree: false,
       contacttype: 'None',
       message: ''
-    })
+    });
+    this.feedbackFormDirective.resetForm();
   }
-  onSubmit() {
-    this.feedback = this.feedbackForm.value;
-    console.log(this.feedback);
-    this.feedbackForm.reset();
-  }
+  
 
   ngOnInit() {
 
