f4ffe8294b2c7a0ffe5950c387a9e2f845be45bd
Yildiz-BO <amamou969@gmail.com>
2020-07-25 20:53:42 +0100

    Template Forms Part 2

--- angular/conFusion/src/app/app.module.ts
+++ angular/conFusion/src/app/app.module.ts
@@ -23,6 +23,10 @@ import { DishDetailsComponent } from './dish-details/dish-details.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -45,7 +49,11 @@ import { MatDialogModule } from '@angular/material/dialog';
     FlexLayoutModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule ,
+    MatCheckboxModule ,
+    FormsModule
   ],
   providers: [
     LeaderService
